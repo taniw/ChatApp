58a662f7223d8e4dbd813777c64486cfded24f0f
taniw <tani.s.wong@gmail.com>
2015-03-02 18:47:24 -0800

    Add posts and can delete them

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,5 +1,19 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope) {
+  // array that holds our posts that were entered into our form
+  $scope.posts = [];
+  // post consists of two attributes
+  $scope.post = {url: 'http://', title: ''};
 
+  // this will add 'post' object into 'posts' array
+  $scope.submitPost = function() {
+  	$scope.posts.push($scope.post);
+  	$scope.post = {url: 'http://', title: ''};
+  };
+
+  // removes an object from the posts array using the index of the object
+  $scope.deletePost = function (index) {
+  	$scope.posts.splice(index, 1);
+  };
 });
